8c9009d8d67b97a6addd99924e1bb53e7b5df596
Paula Kammler <paula@zauberzeug.com>
2025-03-14 19:04:14 +0100

    add option to run mypy and pylint on result

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 from subprocess import CompletedProcess
 
 from pytest_copie.plugin import Copie, Result
@@ -20,7 +21,7 @@ def contains_standard_files(result: Result):
     ]
     all_found = True
     for file in files:
-        if not (result.project_dir / file).is_file():
+        if not ((result.project_dir or Path('')) / file).is_file():
             all_found = False
             print('Standard file was not found in generated project:', file)
     return all_found
@@ -30,17 +31,28 @@ def test_basic_template(copie: Copie):
     result = copie.copy(extra_answers=answers)
     assert result.exit_code == 0
     assert result.exception is None
+    assert result.project_dir is not None
     assert result.project_dir.is_dir()
     assert contains_standard_files(result)
 
 
 def perform_ruff_run(result: Result):
-    ruff_result: CompletedProcess = subprocess.run(['ruff', 'check', result.project_dir],
-                                                   cwd=result.project_dir, check=False)
+    ruff_result: CompletedProcess = subprocess.run(['ruff', 'check', '.'], cwd=result.project_dir, check=False)
     return ruff_result.returncode == 0
 
 
-def test_ruff_runs(copie: Copie):
+def perform_mypy_run(result: Result):
+    mypy_result: CompletedProcess = subprocess.run(['mypy', '.'], cwd=result.project_dir, check=False)
+    return mypy_result.returncode == 0
+
+
+def perform_pylint_run(result: Result):
+    pylint_result: CompletedProcess = subprocess.run(['pylint', '.'], cwd=result.project_dir, check=False)
+    return pylint_result.returncode == 0
+
+
+def test_code_check_runs(copie: Copie):
     result = copie.copy(extra_answers=answers)
     assert result.exit_code == 0
     assert perform_ruff_run(result)
+    assert perform_mypy_run(result)
