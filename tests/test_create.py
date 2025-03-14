80484138f44447534ba98b7d07ac0fa3767da40a
Paula Kammler <paula@zauberzeug.com>
2025-03-14 18:55:13 +0100

    add test to run ruff on project result

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,4 +1,14 @@
+import subprocess
+from subprocess import CompletedProcess
+
 from pytest_copie.plugin import Copie, Result
+
+answers = {
+    'project_name': 'helloworld',
+    'project_description': '',
+    'module_name': 'hello',
+    'python_versions': ['3.9', '3.10', '3.11']
+}
 
 
 def contains_standard_files(result: Result):
@@ -17,11 +27,20 @@ def contains_standard_files(result: Result):
 
 
 def test_basic_template(copie: Copie):
-    result = copie.copy(extra_answers={'project_name': 'helloworld',
-                                       'project_description': '',
-                                       'module_name': 'hello',
-                                       'python_versions': ['3.9', '3.10', '3.11']})
+    result = copie.copy(extra_answers=answers)
     assert result.exit_code == 0
     assert result.exception is None
     assert result.project_dir.is_dir()
     assert contains_standard_files(result)
+
+
+def perform_ruff_run(result: Result):
+    ruff_result: CompletedProcess = subprocess.run(['ruff', 'check', result.project_dir],
+                                                   cwd=result.project_dir, check=False)
+    return ruff_result.returncode == 0
+
+
+def test_ruff_runs(copie: Copie):
+    result = copie.copy(extra_answers=answers)
+    assert result.exit_code == 0
+    assert perform_ruff_run(result)
