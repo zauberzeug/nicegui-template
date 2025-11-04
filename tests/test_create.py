15c7b4c4b5a36b2e8d32fc33896dc6be9a1d3fc7
Paula Kammler <paula@zauberzeug.com>
2025-11-04 10:52:50 +0100

    simplify test

--- tests/test_create.py
+++ tests/test_create.py
@@ -146,9 +146,7 @@ def test_code_checkers(copie: Copie,
                                        'use_poetry': bool(use_poetry),
                                        'use_rosys': bool(use_rosys),
                                        'use_precommit': bool(use_precommit)})
-    arguments = [task]
-    if task != 'pytest':
-        arguments.insert(0, 'make')
+    arguments = [task] if task == 'pytest' else ['make', task]
     # ACT
     test_run: CompletedProcess = subprocess.run(arguments, cwd=result.project_dir, check=False)
     # ASSERT
